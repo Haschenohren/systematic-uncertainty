ccb045266e9aa3579295ec3bc78887b169351fda
Eva Moniz <enmoniz@uncg.edu>
2019-06-15 03:45:36 -0400

    Format data as nice sorted tables

--- retrieve_data.py
+++ retrieve_data.py
@@ -22,22 +22,22 @@ _uncertainty_table = (
 )
 #dictionary for determining column of table based on particle type/charge
 _col_select = {'pospion': 0, 'negpion': 1, 'poskaon': 2, 'negkaon':3, 'posprot': 4, 'negprot': 5}
-def systematic_uncertainty(particle, collision, p_T, value_yield):
+def systematic_uncertainty(particle, collision, p_T, inv_yield):
 	#select a table entry based on particle type/charge, collision, and transverse momentum
 	if collision == 'AuAu':
 		row = 0 if p_T < 3 else 1 if p_T <= 5 else 2
 	else:
 		row = 3 if p_T < 3 else 4
 	col = _col_select[particle]
-	return _uncertainty_table[row][col] * value_yield
-	
+	return _uncertainty_table[row][col] * inv_yield
+
 
 def scrape_filenames(url):
 	#read the html file for scraping
 	response = requests.get(url, verify=False)
 	file_table = html.fromstring(response.content)
 	return file_table.xpath('//tr[position()>1]/td[1]/a/@href')
-	
+
 
 def download_files(base_url, filenames):
 	'''Downloads the list of files to data/ directory'''
@@ -54,11 +54,9 @@ def download_files(base_url, filenames):
 		count += 1
 		print(f'\rDownloaded {count} files.', end='')
 	print()
-	
-	
+
+
 def append_su(filenames):
-	if not os.path.exists('data_org/'):
-		os.mkdir('data_org/')
 	for filename in filenames:
 		#get the particle and collision types from the filename
 		particle, collision = filename.split('_')[:2]
@@ -70,30 +68,73 @@ def append_su(filenames):
 			for row in original.splitlines():
 				cols = row.split()
 				p_T = float(cols[0])
-				value_yield = float(cols[1])
-				su = systematic_uncertainty(particle, collision, p_T, value_yield)
-				f.write(f'{row}\t{su}\n')
+				inv_yield = float(cols[1])
+				su = systematic_uncertainty(particle, collision, p_T, inv_yield)
+				#format with 6 significant figures
+				f.write(f'{row}\t{su:.6g}\n')
 	print('Appended systematic uncertainty column to all files.')
 
+
 def reorganize(filenames):
+	if not os.path.exists('data_org/'):
+		os.mkdir('data_org/')
 	#split filenamess into their 'groups' and centralities
-	pattern = re.compile(r'(\D+)_(cent\d+)\D+')
-	grouped_files = {}
+	pattern = re.compile(r'(\D+)_cent(\d+)\D+')
+	#construct dictionary of lists of files that should be grouped together
+	file_groups = {}
 	for filename in filenames:
 		#extract the group name and the centrality from the filename
 		m = pattern.match(filename)
 		group, centrality = m.group(1,2)
-		if not group in grouped_files:
-			grouped_files[group] = open(f'data_org/{group}.txt', 'w')
-		#concatenate file to the correct grouped file
-		with open(f'data/{filename}', 'r') as infile:
-			outfile = grouped_files[group]
-			outfile.write(f'{centrality}\n')
-			outfile.write(infile.read())
-			outfile.write('\n')
-	for f in grouped_files.values():
-		f.close()
-	print(f'Concatenated {len(filenames)} files into {len(grouped_files)}.')
+		if not group in file_groups:
+			file_groups[group] = [(filename, centrality)]
+		else:
+			file_groups[group].append((filename, centrality))
+	for group, group_files in file_groups.items():
+		write_concatenated_file(group + '.txt', group_files)
+	print(f'Organized {len(filenames)} files into {len(file_groups)}.')
+
+
+def write_concatenated_file(outfile_name, file_centrality_pairs):
+	#Use sort key to sort tuples by filename, and place 0-100% range first in order
+	sort_key = lambda pair: pair[0].replace('0100', '0000')
+	file_centrality_pairs.sort(key=sort_key)
+	outfile = open('data_org/' + outfile_name, 'w')
+	for filename, centrality in file_centrality_pairs:
+		infile = open('data/' + filename, 'r')
+		#construct header for table
+		min_cent = int(centrality[:2])
+		max_cent = int(centrality[-2:])
+		#special case: file ends with 0100 (0-100%)
+		if centrality == '0100':
+			min_cent, max_cent = 0, 100
+		outfile.write(f'Centrality {min_cent}-{max_cent}%\n')
+		#construct and print table
+		table = construct_table(infile.read())
+		outfile.write(table + '\n')
+		infile.close()
+	outfile.close()
+
+
+def construct_table(data_str):
+	#Extract data into 2d list
+	table = [row.split('\t') for row in data_str.splitlines()]
+	#Append table header row to table
+	table.insert(0, ['pT', 'Inv. Yield', 'Stat. Err.', 'Sys. Err'])
+	num_cols = len(table[0])
+	col_widths = [0] * num_cols
+	#Determine column widths by maximum widths of each field in each column
+	for row in table:
+		for c, field in enumerate(row):
+			col_widths[c] = max(col_widths[c], len(field))
+	#Add formatted fields to table str
+	for row in table:
+		for c, field in enumerate(row):
+			w = col_widths[c]
+			#overwrite field with padded field
+			row[c] = f'{field:{w}}'
+	table_str = '\n'.join('  '.join(row) for row in table) + '\n'
+	return table_str
 
 
 if __name__ == '__main__':
